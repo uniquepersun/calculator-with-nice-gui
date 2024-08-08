1f5a4f379ca075a1c32d3d617d1cee13c27488bb
abhay <abhay.i0am@gmail.com>
2024-08-08 19:33:14 +0530

    added more functions only

--- clctr.py
+++ clctr.py
@@ -15,14 +15,17 @@ def check_safe_for_eval(user_input):
         if char not in ALLOWED_CHARS:
             return False 
     return True
-   
-def del_operator():    
-    current = display_entry.get()
-    if len(current) > 0:
-        if current[-1] in OPS:
-            last_index = len(current) -1
-            display_entry.delete(last_index, END)    
-                
+
+def find_last_ops_index(user_input):    
+    last_ops_index = 0
+    chars = list("/-+*&()")
+    replace = list(user_input)
+    for c in replace:
+        if c in chars:
+            if replace.index(c) > last_ops_index:
+                last_ops_index = replace.index(c)
+                replace[replace.index(c)] = "removed"
+    return last_ops_index
 
 def update_input_ready_status(func=False):    
     global Prepare_for_New_input   
@@ -30,6 +33,16 @@ def update_input_ready_status(func=False):
         Prepare_for_New_input = True
     else:
         Prepare_for_New_input = False 
+        
+def del_operator():    
+    current = display_entry.get()
+    if len(current) > 0:
+        if current[-1] in OPS:
+            last_index = len(current) -1
+            display_entry.delete(last_index, END)    
+                
+                
+
 
 
 def insert_multiplication():   
@@ -45,13 +58,78 @@ def number_input(num):
         display_entry.delete(0, END)      
     update_input_ready_status()   
     display_entry.insert(END, num)
-
-
+    
 def operator_button(operator):
     del_operator()
     update_input_ready_status()
     display_entry.insert(END, operator)
     
+    
+
+
+def point():
+    update_input_ready_status()
+    display_entry.insert(END, ".")
+def clear():
+    update_input_ready_status()
+    display_entry.delete(0, END)
+    
+    
+   
+def signchange():
+    update_input_ready_status()
+    current = display_entry.get()
+    last_index = find_last_ops_index(current)    
+    if len(current) > 0 and len(current)-1 != last_index and last_index > 0:      
+        if last_index > 0:
+            if current[last_index] == "*":
+                display_entry.insert(last_index + 1, "(-")
+            elif current[last_index] == "-":
+                display_entry.delete(last_index)
+                display_entry.insert(last_index, "+")
+            elif current[last_index] == "+":
+                display_entry.delete(last_index)
+                display_entry.insert(last_index, "-")   
+            elif current[last_index] == "(":
+                display_entry.insert(last_index + 1, "-")
+    elif len(current)-1 == last_index and last_index > 0:
+        if current[last_index] == "-":
+            display_entry.delete(last_index)
+            display_entry.insert(last_index, "+")
+        elif current[last_index] == "(":
+            display_entry.insert(last_index + 1, "-")    
+        else:
+            display_entry.delete(last_index)
+            display_entry.insert(last_index, "-")    
+
+    else:
+        if current[0] != "-":
+            display_entry.insert(0, "-")
+        elif current[0] == "-":
+            display_entry.delete(0)      
+
+
+def parentheses():             
+    update_input_ready_status()
+    current = display_entry.get()
+    left = current.count("(")
+    right = current.count(")")
+    if current == "":
+        display_entry.insert(END, "(")
+    elif current[-1] == "(":
+        display_entry.insert(END, "(")
+    elif current[-1] in OPS:
+        display_entry.insert(END, "(")
+    elif left > right:
+        display_entry.insert(END, ")")
+    elif left == right:
+        if current[-1] in OPS:    
+            display_entry.insert(END, "(")    
+        else:
+            display_entry.insert(END, "*(")    
+    else:
+        display_entry.insert(END, "error")
+  
 
 window = Tk()
 window.config(padx=15, pady=15, bg=COLORS[0], highlightthickness=0)

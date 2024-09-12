bda04d162e88be33ed42083f653671bc5ce3acef
abhay <abhay.i0am@gmail.com>
2024-09-12 20:00:08 +0530

    modified:   clctr.py

--- clctr.py
+++ clctr.py
@@ -236,11 +236,11 @@ button_frame.grid(column=0,row=2)
 
 clear_button = Button(button_frame, text="C", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=clear, bg=COLORS[1], fg=COLORS[2])
 clear_button.grid(column=0, row=0)
-parentheses_button = Button(button_frame, text="( )", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=parentheses, bg=COLORS[2], fg=COLORS[2])
+parentheses_button = Button(button_frame, text="( )", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=parentheses, bg=COLORS[1], fg=COLORS[2])
 parentheses_button.grid(column=1, row=0)
-percent_button = Button(button_frame, text="%", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("%"), bg=COLORS[2], fg=COLORS[2])
+percent_button = Button(button_frame, text="%", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("%"), bg=COLORS[1], fg=COLORS[2])
 percent_button.grid(column=2, row=0)
-division_button = Button(button_frame, text="/", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("/"), bg=COLORS[2], fg=COLORS[2])
+division_button = Button(button_frame, text="/", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("/"), bg=COLORS[1], fg=COLORS[2])
 division_button.grid(column=3, row=0)
 
 seven_button = Button(button_frame, text="7", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("7"), bg=COLORS[2], fg=COLORS[0])
@@ -249,7 +249,7 @@ eight_button = Button(button_frame, text="8", font=BUTTON_FONT, width=3,height=1
 eight_button.grid(column=1, row=1)
 nine_button = Button(button_frame, text="9", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("9"), bg=COLORS[2], fg=COLORS[0])
 nine_button.grid(column=2, row=1)
-multiplication_button = Button(button_frame, text="*", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("*"), bg=COLORS[2], fg=COLORS[2])
+multiplication_button = Button(button_frame, text="*", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("*"), bg=COLORS[2], fg=COLORS[0])
 multiplication_button.grid(column=3, row=1)
 
 
@@ -259,7 +259,7 @@ five_button = Button(button_frame, text="5", font=BUTTON_FONT, width=3,height=1,
 five_button.grid(column=1, row=2)
 six_button = Button(button_frame, text="6", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("6"), bg=COLORS[3], fg=COLORS[0])
 six_button.grid(column=2, row=2)
-subtraction_button = Button(button_frame, text="-", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("-"), bg=COLORS[2], fg=COLORS[2])
+subtraction_button = Button(button_frame, text="-", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("-"), bg=COLORS[2], fg=COLORS[0])
 subtraction_button.grid(column=3, row=2)
 
 one_button = Button(button_frame, text="1", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("1"), bg=COLORS[3], fg=COLORS[0])
@@ -268,7 +268,7 @@ two_button = Button(button_frame, text="2", font=BUTTON_FONT, width=3,height=1, 
 two_button.grid(column=1, row=3)
 three_button = Button(button_frame, text="3", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("3"), bg=COLORS[3], fg=COLORS[0])
 three_button.grid(column=2, row=3)
-addition_button = Button(button_frame, text="+", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("+"), bg=COLORS[2], fg=COLORS[2])
+addition_button = Button(button_frame, text="+", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("+"), bg=COLORS[2], fg=COLORS[0])
 addition_button.grid(column=3, row=3)
 
 
